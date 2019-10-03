7b2630d41b882d872e1fc2bc1d367c79668952af
Nikita <44435714+Nickpicnic@users.noreply.github.com>
2019-10-04 01:38:19 +0300

    Update findLongestSubstring.js

--- Problem_Solving_Patterns/findLongestSubstring.js
+++ Problem_Solving_Patterns/findLongestSubstring.js
@@ -1,30 +1,34 @@
 function findLongestSubstring(str){
-    let min = 0,
-        max = 0,
+    let max = 0,
         substring = str[max],
         len = 0,
         currentLen = 1;
 
         if (str.length) {
             while (max < str.length) {
-                if (substring.length === (new Set(substring).length)) {
+                if (substring.length === (new Set(substring).size)) {
                     max++;
                     substring += str[max];
                     currentLen++;
                     if (currentLen > len) {
                         len = currentLen;
                     }
-                    console.log(substring);
+                    // console.log(substring);
                 } else {
                     substring = substring.slice(1);
                     currentLen--;
-                    min++;
+                    // console.log(substring);
                 }
             }
-            return len
+            return len - 1
         }
         return 0
 }
   
-findLongestSubstring('');   // 0
-findLongestSubstring('rithmschool');   // 7
+console.log(findLongestSubstring(''));   // 0
+console.log(findLongestSubstring('rithmschool'));   // 7
+console.log(findLongestSubstring('thisisawesome'));   // 6
+console.log(findLongestSubstring('thecatinthehat'));   // 7
+console.log(findLongestSubstring('bbbbbbbb'));   // 1
+console.log(findLongestSubstring('longestsubstring'));   // 8
+console.log(findLongestSubstring('thisishowwedoit'));   // 6
